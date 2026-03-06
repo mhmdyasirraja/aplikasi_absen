5d46e0c36cf0f0342eab52384daa7f2151d55169
mhmdyasirraja <mhmdyasir00@gmail.com>
2026-03-06 09:51:26 +0700

    update readme dan penambahan file baru test07.php

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <h2>Halaman Kontak</h2>
+    <p>Silahkan menghubungi kami untuk informasi penyewaan baju.</p>
+    <p>Email: tokosewabaju@email.com</p>
+    <p>WhatsApp: 08123456789</p>
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <h1>Selamat Datang di Toko Sewa Baju Terbaik  {{$nama}}</h1>
+    <p>anda adalah seorang  {{$pekerjaan}}</p>
+</div>
